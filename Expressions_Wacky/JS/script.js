9ff1ec12d92f17110eda14ee24143890740a73a7
martinjlemay <martinjlemay@gmail.com>
2014-06-13 00:09:18 -0500

    final revisions done and comments added

--- Expressions_Wacky/JS/script.js
+++ Expressions_Wacky/JS/script.js
@@ -1,12 +1,16 @@
 //Martin Lemay 6/12/2014 Expressions_Wacky
 
-// How many eggs and pancakes you need for all of your guests for breakfast
+// How many eggs and pancakes and toast you need for all of your guests for breakfast
 
 
-var adults= prompt("How many adults are coming?");
-var children= prompt("How many children are coming?");
-var eggsNeeded= (adults * 3) + (children * 2);
-var pancakesNeeded= (adults * 3) + (children * 2);
+var adults= prompt("How many adults are eating?");// Asks how many adults are eating.
+var children= prompt("How many children are eating?");//Asks how many children are eating.
+var eggsNeeded= (adults * 3) + (children * 2); /*Average of 3 eggs per adult times how many adults and 2 eggs per child
+times the number of children equalling eggs needed.*/
+var pancakesNeeded= (adults * 4) + (children * 3);/*Average of 4 pancakes per adult times how many adults plus
+3 pancakes per child times the number of children equalling pancakes needed.*/
+var toastNeeded= (adults *2) + (children*2);/*Average of 2 pcs of toast per adult times how many adults added to
+ 2 pcs of toast per child times the number of children equalling eggs needed.*/
 
 console.log("If " + adults + " adults and " + children + " children are coming for breakfast. \nYou will need " +
-eggsNeeded + " eggs and " + pancakesNeeded + " pancakes.")
+eggsNeeded + " eggs and " + pancakesNeeded + " pancakes and " + toastNeeded + "pieces of toast");
