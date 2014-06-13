5bfd1de28b9aa42af62d0b3511b4fe4e695c40ea
martinjlemay <martinjlemay@gmail.com>
2014-06-12 22:20:11 -0500

    updated code

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -6,7 +6,10 @@ var feetOfRG6 = prompt("Please enter how many ft of cable"); // How many ft of c
 var frequency = prompt("Please enter 100,400,700, or 900mhz"); //List of available frequencies
 var lossPerFoot100 = .02; //Loss per foot at 100mhz
 var lossPerFoot400 = .043; //Loss per foot at 400mhz
-
+var frequency1 = frequency("100");
+var frequency2 = frequency("400");
+var frequency3 = frequency("700");
+var frequency4 = frequency("900");
 var lossPerFoot700 = .056; //Loss per foot at 700mhz
 var lossPerFoot900 = .06; //Loss per foot at 900mhz
 
@@ -15,3 +18,9 @@ var TotalLoss400 = feetOfRG6 * lossPerFoot400; // Feet of cable times the loss p
 var TotalLoss700 = feetOfRG6 * lossPerFoot700; // Feet of cable times the loss per foot at 700mhz
 var TotalLoss900 = feetOfRG6 * lossPerFoot900; // Feet of cable times the loss per foot at 900mhz
 
+console.log("At " + frequency1 + "mhz with a length of " + feetOfRG6 + "ft, RG6 coax cable loses "
+    + TotalLoss100 + "db of signal" + "\nAt " + frequency2 + "mhz with a length of " + feetOfRG6 + "ft, " +
+    "RG6 coax cable loses " + TotalLoss400 + "db of signal\nAt " + frequency3 + " with a length of " + feetOfRG6 +
+    "RG6 coax cable loses " + TotalLoss700 + "db of signal" + "\nAt " +frequency4 + "mhz with a length of " +
+    feetOfRG6 +  "ft, RG6 coax cable loses " + TotalLoss900 + "db of signal");
+/* EX..At 100 mhz with a length of 100 ft, RG6 coax cable loses 2db of signal */
