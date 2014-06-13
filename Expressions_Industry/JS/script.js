2a79c669c185a85cce3cf730fb3d0a8b1059067a
martinjlemay <martinjlemay@gmail.com>
2014-06-12 22:00:36 -0500

    started code

---
+++ Expressions_Industry/JS/script.js
@@ -0,0 +1,9 @@
+//Martin Lemay 6/12/2014 Expressions Assignment
+
+// This is a calculator for the amount of signal loss for RG6 Coax Cable for a cable technician
+
+var feetOfRG6 = promt("Please enter how many ft of cable"); // How many ft of cable used?
+var frequency = ; //List of available frequencies
+var lossPerFoot100 = .02; //Loss per foot at 100mhz
+var lossPerFoot400 = .043; //Loss per foot at 400mhz
+
