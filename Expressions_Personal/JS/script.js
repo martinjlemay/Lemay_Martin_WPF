e8c8112d5821634f43238d8c44f0a8c406062c17
martinjlemay <martinjlemay@gmail.com>
2014-06-12 23:04:10 -0500

    added comments to expressions personal

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -1,13 +1,14 @@
 //Martin Lemay 6/12/2014 Expressions_Personal Assignment
 
-//This is the average of a bowlers games.
+//This is the average of a bowlers league night 3 games.
 
 var firstName= prompt("What is your first name?");
 var lastName= prompt("What is your last name?");
-var game1= prompt("Final score of game 1?");
-var game2= prompt("Final score of game 2?");
-var game3= prompt("Final score of game 3?");
-var totalOfAllGames= Number(game1) + Number(game2) + Number(game3);
+var game1= prompt("Final score of game 1?");//score of game 1
+var game2= prompt("Final score of game 2?");//score of game 2
+var game3= prompt("Final score of game 3?");//score of game 3
+var totalOfAllGames= Number(game1) + Number(game2) + Number(game3);// all 3 games added together
+var average= totalOfAllGames / 3; //total of all 3 games divided by the amount of 3 games played
 
 
-console.log(totalOfAllGames)
+console.log("totalOfAllGames)
