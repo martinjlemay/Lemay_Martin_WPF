336b17af79490f99e4f638e6b31a33dc85636cef
martinjlemay <martinjlemay@gmail.com>
2014-06-12 22:10:16 -0500

    added code and adjustments 4

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -12,4 +12,6 @@ var lossPerFoot900 = .06; //Loss per foot at 900mhz
 
 var TotalLoss100 = feetOfRG6 * lossPerFoot100; // Feet of cable times the loss per foot at 100mhz
 var TotalLoss400 = feetOfRG6 * lossPerFoot400; // Feet of cable times the loss per foot at 400mhz
+var TotalLoss700 = feetOfRG6 * lossPerFoot700; // Feet of cable times the loss per foot at 700mhz
+var TotalLoss900 = feetOfRG6 * lossPerFoot900; // Feet of cable times the loss per foot at 900mhz
 
