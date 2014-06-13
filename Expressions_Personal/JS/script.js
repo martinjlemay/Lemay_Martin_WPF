552ce30b492172ae2516194a8e5fd87f3a5e5c01
martinjlemay <martinjlemay@gmail.com>
2014-06-12 22:58:43 -0500

    checking code

---
+++ Expressions_Personal/JS/script.js
@@ -0,0 +1,13 @@
+//Martin Lemay 6/12/2014 Expressions_Personal Assignment
+
+//This is the average of a bowlers games.
+
+var firstName= prompt("What is your first name?");
+var lastName= prompt("What is your last name?");
+var game1= prompt("Final score of game 1?");
+var game2= prompt("Final score of game 2?");
+var game3= prompt("Final score of game 3?");
+var totalOfAllGames= Number(game1) + Number(game2) + Number(game3);
+
+
+console.log(totalOfAllGames)
