86fe30a87a8eccaa3ec67130c16f5e722be34564
martinjlemay <martinjlemay@gmail.com>
2014-06-19 21:57:58 -0500

    start of code for conditionals personal

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -1,3 +1,26 @@
 /**
- * Created by martinjlemay on 6/19/14.
+ * Created by Martin Lemay on 6/19/14. Conditionals_Personal
  */
+
+// Going to the movies or Chuck-E-Cheese or Miniature golf
+
+var rain= prompt("Is it raining? Please type \'Yes\' or \'No\' ");
+var babySitter= prompt("Is a babysitter available. \'Yes\' or \'No\' ");
+
+
+if (babySitter === "Yes"){
+    if (rain === "No"){
+        console.log("We will be going to play miniature golf.");
+    }else if(rain === "Yes"){
+        console.log("We will be going to the movies.");
+    }
+    else{
+
+    }
+}
+if (babySitter === "No"){
+    console.log("We will take our child to Chuck-E-Cheese.");
+}
+
+
+
