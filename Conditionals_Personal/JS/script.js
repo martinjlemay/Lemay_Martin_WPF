a32a24225fc2ec033c8b9bf86c3b89ca169292fd
martinjlemay <martinjlemay@gmail.com>
2014-06-19 22:08:20 -0500

    added notes to conditionals personal

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -4,21 +4,19 @@
 
 // Going to the movies or Chuck-E-Cheese or Miniature golf
 
-var rain= prompt("Is it raining? Please type \'yes\' or \'no\' ");
-var babySitter= prompt("Is a babysitter available. \'yes\' or \'no\' ");
+var rain= prompt("Is it raining? Please type \'yes\' or \'no\' "); //Asks if it is raining out.
+var babySitter= prompt("Is a babysitter available. \'yes\' or \'no\' "); //Asks if there is a babysitter available
 
-if (babySitter == "yes"){
+if (babySitter == "yes"){ //if a babysitter is a yes and no rain go miniature golfing
     if (rain == "no"){
         console.log("We will be going to play miniature golf.");
-    }else if(rain == "yes"){
+    }else if(rain == "yes"){  //if a babysitter is yes and rain is yes go to movies
         console.log("We will be going to the movies.");
-    }
-    else{
     }
 }
 if (babySitter == "no"){
     console.log("We will take our child to Chuck-E-Cheese.");
-}
+}// if no babysitter then we go to chuck-e-cheese.
 
 
 
