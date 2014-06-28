a079c1c94d10385c37eb2d764fc83002c7d21ba8
martinjlemay <martinjlemay@gmail.com>
2014-06-28 06:16:18 -0500

    updates to code

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -3,24 +3,28 @@
  */
 // Function_Personal
 
-firstName= prompt("What is your first name?");
-lastName= prompt("What is your last name?");
+/*
 game1= prompt("Final score of game 1?");//score of game 1
 game2= prompt("Final score of game 2?");//score of game 2
 game3= prompt("Final score of game 3?");//score of game 3
 var gameAverage = Number(game1) + Number(game2) + Number(game3) /3;
 totalOfAllGames = Number(game1) + Number(game2) + Number(game3);
-
-var result = bowler1(firstName,lastName,totalOfAllGames,gameAverage);
-var bowler1= function(firstName, lastName,totalOfAllGames,gameAverage){
-    var game1= prompt("Final score of game 1?");//score of game 1
-    var game2= prompt("Final score of game 2?");//score of game 2
-    var game3= prompt("Final score of game 3?");//score of game 3
-    totalOfAllGames = Number(game1) + Number(game2) + Number(game3);
-    gameAverage = Number(game1) + Number(game2) + Number(game3) /3;
-    return bowler(firstName,lastName,totalOfAllGames,gameAverage)
+*/
 
 
+var firstName= prompt("What is your first name?");
+var lastName= prompt("What is your last name?");
+
+var bowler1;
+var game1= prompt("Final score of game 1?");//score of game 1
+var game2= prompt("Final score of game 2?");//score of game 2
+var game3= prompt("Final score of game 3?");//score of game 3
+var totalOfAllGames = Number(game1) + Number(game2) + Number(game3);
+var average = totalOfAllGames / 3;
+bowler1= function(firstName, lastName, totalOfAllGames, average) {
+    average= totalOfAllGames /3;
+    totalOfAllGames = game1 + game2 + game3;
+    return bowler1(firstName, lastName, totalOfAllGames, average);
 
 };
-console.log(result)
+console.log(bowler1(firstName,lastName,totalOfAllGames,average));
