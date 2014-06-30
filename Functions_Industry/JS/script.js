7b8a3742c57b3977cb3ec461efa63fdab1c44104
martinjlemay <martinjlemay@gmail.com>
2014-06-29 22:36:11 -0500

    adjustments to industry

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -4,7 +4,7 @@
  */
 
 
-// Aquarium building guide lines for rectangle tank
+// Aquarium building guide lines for rectangle tank tanks with dimensions between the sizes 15" x 24" and 33" x 96"
 
 var l = prompt("Home many inches long will tank be");
 var w = prompt("How many inches wide will tank be");
@@ -12,8 +12,16 @@ var h = prompt("How many inches tall will tank be");
 var totalGallons = volume(l,w,h);
 
 function volume(l,w,h){
-    gallons = (l * w * h) /231;
+    gallons = Math.round((l * w * h) /231);
     return gallons;
 
 }
-console.log(totalGallons);
+console.log("Your aquarium will hold " + totalGallons + " gallons of water.");
+   if (h < 18){
+       console.log("Builder can use 6mm or thicker glass.");
+   }else if(h >18 <= 24){
+        console.log("Builder must use 9mm or thicker glass");
+   }else if(h >24 <=27){
+            console.log("Builder must use 12mm or thicker glass");
+   }else{console.log("Builder must use 16mm or thicker glass")
+   }
