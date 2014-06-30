aa335a966766d031a70d7ba64c6cda55ba43366a
martinjlemay <martinjlemay@gmail.com>
2014-06-29 23:15:36 -0500

    had to add comments

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -5,11 +5,11 @@
 
 //******** Rock, Paper, Scissors *******
 
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-    while(userChoice !="rock" && userChoice != "paper" && userChoice != "scissors"){
-        userChoice= prompt("Please type in rock paper or scissors in lower case only Only!")
+var userChoice = prompt("Do you choose rock, paper or scissors?");//asks user rock,paper, or scissors
+    while(userChoice !="rock" && userChoice != "paper" && userChoice != "scissors"){ //validation
+        userChoice= prompt("Please type in rock paper or scissors in lower case only Only!")//Re-prompt user input
     }
-var computerChoice = Math.random();
+var computerChoice = Math.random();//computer prints random number to determine rock,paper,or scissor
     if (computerChoice < 0.34) {
        computerChoice = "rock";
     } else if(computerChoice <= 0.67) {
@@ -18,7 +18,8 @@ var computerChoice = Math.random();
         computerChoice = "scissors";
     }
         console.log("Computer: " + computerChoice);
-var compare = function(userChoice, computerChoice){
+var compare = function(userChoice, computerChoice)
+{//function to determine user choice and computer choice winner parameters
     if (userChoice === computerChoice){
         console.log("The result is a tie!");
     }
@@ -30,7 +31,8 @@ var compare = function(userChoice, computerChoice){
         }
     }
 };
-compare(userChoice,computerChoice);{
+compare(userChoice,computerChoice);
+{//function to determine user choice and computer choice winner parameters
     if (!(userChoice !== "paper")) {
         if (!(computerChoice !== "rock")) {
             console.log("paper wins");
@@ -39,7 +41,8 @@ compare(userChoice,computerChoice);{
         }
     }
 }
-compare(userChoice,computerChoice);{
+compare(userChoice,computerChoice);
+{//function to determine user choice and computer choice winner parameters
     if(userChoice==="scissors"){
         if(computerChoice==="rock"){
             console.log ("rock wins");
