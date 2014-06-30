ea5aad11c2426d3a6fa430bca8be336322d006ad
martinjlemay <martinjlemay@gmail.com>
2014-06-29 21:52:50 -0500

    start of fish tank calc

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -2,3 +2,18 @@
  * Created by Martin Lemay on 6/24/14.
  * Functions_Industry
  */
+
+
+// Aquarium building guide lines for rectangle tank
+
+var l = prompt("Home many inches long will tank be");
+var w = prompt("How many inches wide will tank be");
+var h = prompt("How many inches tall will tank be");
+var totalGallons = volume(l,w,h);
+
+function volume(l,w,h){
+    gallons = (l * w * h) /231;
+    return gallons;
+
+}
+console.log(totalGallons);
