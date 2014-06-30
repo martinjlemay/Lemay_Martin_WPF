66cfb4392d086585e3b2b9d718ec5a6785db167b
martinjlemay <martinjlemay@gmail.com>
2014-06-29 23:10:05 -0500

    committed last comments to git

--- Archive/Functions_Personal/JS/script.js
+++ Archive/Functions_Personal/JS/script.js
@@ -16,12 +16,12 @@ game3=prompt("bowler 1 score of game 3?");
 
 var bowler1= bowler(game1, game2, game3);
 
-function bowler(game1, game2, game3){//function to get average of each bowler
+function bowler(game1, game2, game3){
     totalScore= Number(game1)+ Number(game2)+ Number(game3);
     average= totalScore /3;
     return average;
 }
-console.log(firstName + " " + lastName +"\'s average = ", bowler1);//prints bowler 1 's name and average to console
+console.log(firstName + " " + lastName +"\'s average = ", bowler1);
 
 firstName=prompt("Type first name of bowler 2");
 lastName=prompt("Type last name of bowler 2");
@@ -31,7 +31,7 @@ game3=prompt("bowler 2 score of game 3?");
 
 var bowler2= bowler(game1, game2, game3);
 
-console.log(firstName + " " + lastName +"\'s average = ", bowler2);//prints bowler 2 's name and average to console
+console.log(firstName + " " + lastName +"\'s average = ", bowler2);
 
 firstName=prompt("Type first name of bowler 3");
 lastName=prompt("Type last name of bowler 3");
@@ -41,8 +41,8 @@ game3=prompt("bowler 3 score of game 3?");
 
 var bowler3= bowler(game1, game2, game3);
 
-console.log(firstName + " " + lastName +"\' average = ", bowler3);//prints bowler 3 's name and average to console
+console.log(firstName + " " + lastName +"\' average = ", bowler3);
 
-console.log("The team\'s average is ", (bowler1 + bowler2 + bowler3)/3 );..//prints team's name and average to console
+console.log("The team\'s average is ", (bowler1 + bowler2 + bowler3)/3 );
 
 
---
+++ Lemay_Martin_Functions/Functions_Wacky/JS/script.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Martin Lemay on 6/25/14.
+ * Function_Wacky
+ */
+
+//******** Rock, Paper, Scissors *******
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+    while(userChoice !="rock" && userChoice != "paper" && userChoice != "scissors"){
+        userChoice= prompt("Please type in rock paper or scissors in lower case only Only!")
+    }
+var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+       computerChoice = "rock";
+    } else if(computerChoice <= 0.67) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+        console.log("Computer: " + computerChoice);
+var compare = function(userChoice, computerChoice){
+    if (userChoice === computerChoice){
+        console.log("The result is a tie!");
+    }
+    else if (userChoice === "rock"){
+        if(computerChoice === "scissors"){
+            console.log ("rock wins");
+        }else{
+            console.log ("paper wins");
+        }
+    }
+};
+compare(userChoice,computerChoice);{
+    if (!(userChoice !== "paper")) {
+        if (!(computerChoice !== "rock")) {
+            console.log("paper wins");
+        } else {
+            console.log("scissors wins");
+        }
+    }
+}
+compare(userChoice,computerChoice);{
+    if(userChoice==="scissors"){
+        if(computerChoice==="rock"){
+            console.log ("rock wins");
+        }else{
+            console.log("scissors win");
+        }
+    }
+}
+
