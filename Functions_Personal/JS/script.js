cdf60bd1bad5e5474f40b849c70ce80f324197ee
martinjlemay <martinjlemay@gmail.com>
2014-06-29 21:40:15 -0500

    fixes to bowler info

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -3,6 +3,10 @@
  */
 // Function_Personal
 
+// How to average a 3 bowler team.
+
+
+
 
 firstName=prompt("Type first name of bowler 1");
 lastName=prompt("Type last name of bowler 1");
@@ -10,8 +14,6 @@ game1=prompt("bowler 1 score of game 1?");
 game2=prompt("bowler 1 score of game 2?");
 game3=prompt("bowler 1 score of game 3?");
 
-
-}
 var bowler1= bowler(game1, game2, game3);
 
 function bowler(game1, game2, game3){
@@ -19,7 +21,7 @@ function bowler(game1, game2, game3){
     average= totalScore /3;
     return average;
 }
-console.log("bowler 1 average = ", bowler1);
+console.log(firstName + " " + lastName +"\'s average = ", bowler1);
 
 firstName=prompt("Type first name of bowler 2");
 lastName=prompt("Type last name of bowler 2");
@@ -29,7 +31,7 @@ game3=prompt("bowler 2 score of game 3?");
 
 var bowler2= bowler(game1, game2, game3);
 
-console.log("bowler 2 average = ", bowler2);
+console.log(firstName + " " + lastName +"\'s average = ", bowler2);
 
 firstName=prompt("Type first name of bowler 3");
 lastName=prompt("Type last name of bowler 3");
@@ -39,8 +41,8 @@ game3=prompt("bowler 3 score of game 3?");
 
 var bowler3= bowler(game1, game2, game3);
 
-console.log("bowler 3 average = ", bowler3);
+console.log(firstName + " " + lastName +"\' average = ", bowler3);
 
-console.log("team average = ", (bowler1 + bowler2 + bowler3)/3 );
+console.log("The team\'s average is ", (bowler1 + bowler2 + bowler3)/3 );
 
 
